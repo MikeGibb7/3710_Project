6635011059eabc0e770c192fa45ebc19537bf86b
Aiden25451 <a.asprakis@gmail.com>
2026-02-18 23:06:11 -0500

    competition is now working

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from strategyFunctions import moveHistoryNumberToString
+from strategyFunctions import calculateScore, generateRandomStrategy, printStrategy
 
-print(moveHistoryNumberToString(63, 3))
+rounds_played = 3
+
+firstBot = generateRandomStrategy(rounds_played)
+secondBot = generateRandomStrategy(rounds_played)
+print("Robot1: ")
+printStrategy(firstBot, rounds_played)
+print()
+print("Robot2: ")
+printStrategy(secondBot, rounds_played)
+print()
+print("Battle")
+calculateScore(firstBot, secondBot, 3, 0, 5, 1, rounds_played)
+ 
